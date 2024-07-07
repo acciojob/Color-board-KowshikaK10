745a6856865e3eae73f59bc0355c31969bd3cd27
KowshikaK10 <139222757+KowshikaK10@users.noreply.github.com>
2024-07-07 19:55:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+const container=document.querySelector('.container');
+for(let i=1;i<=800;i++){
+	let square=`<div class='square'></div>`
+	container.innerHTML+=square
+}
+
+const squares=document.querySelectorAll('.square');
+	squares.forEach(square=>{
+		square.addEventListener('mouseover',()=>{
+			square.style.backgroundColor=randomColor();
+		});
+		square.addEventListener('mouseout',()=>{
+			setTimeout(() => {			  
+				square.style.backgroundColor='';
+			}, 1000);
+		});
+})
+
+function randomColor(){
+	let color='';
+	for (var i=0; i<6; i++) {
+        color += (Math.random()*16|0).toString(16);
+    }
+    return '#'+color;
+}
